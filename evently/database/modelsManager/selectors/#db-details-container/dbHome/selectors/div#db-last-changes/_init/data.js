7948addf56bc6b22b6d8f587e608a0ac9388b1d3
mchouet <mchouet@2504b9b7-a859-4e63-a36b-a611b0752273>
2013-04-08 09:57:33 +0000

    Added #2858
    git-svn-id: https://amap-dev.cirad.fr/svn/p2pnote/trunk@2176 2504b9b7-a859-4e63-a36b-a611b0752273

--- evently/database/modelsManager/selectors/#db-details-container/dbHome/selectors/div#db-last-changes/_init/data.js
+++ evently/database/modelsManager/selectors/#db-details-container/dbHome/selectors/div#db-last-changes/_init/data.js
@@ -1,12 +1,17 @@
 function(lastChanges) {
-    var utilsLib = $$(this).app.getlib('utils');
-    
+    var app = $$(this).app,
+        utilsLib = app.getlib('utils'),
+        cacheLib = app.getlib('cache');
+
     var changes = lastChanges.rows.map(function(e) {
+        $.log('e', e);
         return { 
             id: e.id,
             time: utilsLib.formatDateTime(e.value.time),
             author: e.value.author,
-            label: e.value.label
+            label: e.value.label,
+            structname: (e.value.mmId ? cacheLib.get_cached_mm(app, e.value.mmId).name : ''),
+            modname: (e.value.modi ? cacheLib.get_name(app, e.value.mmId, e.value.modi) : 'special doc')
         };
     });
 
--- views/last_changes/map.js
+++ views/last_changes/map.js
@@ -3,7 +3,6 @@ function (doc) {
     if(doc.$meta) {
         var time = doc.$meta.edited_at || doc.$meta.created_at,
         author = doc.$meta.edited_by || doc.$meta.created_by;
-        emit(Date.parse(time), {author:author, time:time, label:doc.$label});
+        emit(Date.parse(time), { author:author, time:time, label:doc.$label, mmId: doc.$mm, modi: doc.$modi });
     }
-    
 }
