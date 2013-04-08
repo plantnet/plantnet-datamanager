82805bdebbad69cab0bff6ea4c559be34686cca8
mchouet <mchouet@2504b9b7-a859-4e63-a36b-a611b0752273>
2013-04-08 15:23:15 +0000

    Fixed new module renaming in editStructure Fixed #2862 - creating a ref field pointing to nothing is now forbidden
    git-svn-id: https://amap-dev.cirad.fr/svn/p2pnote/trunk@2181 2504b9b7-a859-4e63-a36b-a611b0752273

--- evently/database/modelsManager/selectors/#db-details-container/editModel/selectors/div#mmmodules/_init/selectors/div.module/_init/selectors/input.modname/change.js
+++ evently/database/modelsManager/selectors/#db-details-container/editModel/selectors/div#mmmodules/_init/selectors/div.module/_init/selectors/input.modname/change.js
@@ -2,10 +2,12 @@ function() {
     var mm = $$(this).app.data.mm,
         modt = $(this).parent().attr('id');
 
-    mm.modules[modt].name = $(this).val();
-
-    var tabPos = $('#model-editor-modules-tabs li.active').index();
-
-    $('#mmstructure').trigger('_init');
-    $('#mmmodules').trigger('_init', { activateTab: tabPos });
+    if (mm.modules[modt]) {
+        mm.modules[modt].name = $(this).val();
+        var tabPos = $('#model-editor-modules-tabs li.active').index();
+        $('#mmstructure').trigger('_init');
+        $('#mmmodules').trigger('_init', { activateTab: tabPos });
+    } else { // when the module is brand new, there is no mm.modules[modt] yet - just change the display
+        $('#model-editor-modules-tabs li.active a').html($(this).val());
+    }
 };
