14bf556c15021cdedf9810485c7ef78d521ad9a1
mchouet <mchouet@2504b9b7-a859-4e63-a36b-a611b0752273>
2013-04-18 15:15:32 +0000

    * Updated jQuery-ui to version 1.10.2
    git-svn-id: https://amap-dev.cirad.fr/svn/p2pnote/trunk@2199 2504b9b7-a859-4e63-a36b-a611b0752273

--- vendor/datamanager/_attachments/loader.js
+++ vendor/datamanager/_attachments/loader.js
@@ -5,11 +5,12 @@ function loadLib(scripts) {
 };
 
 loadLib([
-    'vendor/jquery-ui/jquery-ui-1.8.24.custom.min.js',
+    'vendor/jquery-ui/jquery-ui-1.10.2.custom.min.js',
     'vendor/jquery/form/jquery.form.js',
     'vendor/jquery/autocomplete/jquery.autocomplete.min.js',
     'vendor/jquery/colorbox/jquery.colorbox.min.js',
     'vendor/jquery-ui/timepicker/jquery-ui.timepicker.js',
     'vendor/jquery/imagesloaded/jquery.imagesloaded.min.js',
+    //'vendor/bootstrap/bootstrap-datepicker/bootstrap-datepicker.js',
     'vendor/jquery/exif/jquery.exif.js'
 ]);
