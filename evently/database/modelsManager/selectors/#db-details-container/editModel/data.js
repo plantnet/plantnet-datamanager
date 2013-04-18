92c14ca97366fedc5fb109f0747005e6e0e13ca0
mchouet <mchouet@2504b9b7-a859-4e63-a36b-a611b0752273>
2013-04-18 16:13:00 +0000

    Changed display "structure" / "dictionary" when editing
    git-svn-id: https://amap-dev.cirad.fr/svn/p2pnote/trunk@2205 2504b9b7-a859-4e63-a36b-a611b0752273

--- evently/database/modelsManager/selectors/#db-details-container/editModel/data.js
+++ evently/database/modelsManager/selectors/#db-details-container/editModel/data.js
@@ -2,11 +2,13 @@ function(mm, mms) {
     var app = $$(this).app;
 
     app.data.mm = mm;
-    app.data.orig_mm_json = JSON.stringify(mm); // deep copy
     app.data.mms = mms;
 
     // marker to warn user before quitting structure editor
     app.data.structureEditorOpen = true;
 
-    return {mm: mm};
+    return {
+        mm: mm,
+        isref: mm.isref
+    };
 };
