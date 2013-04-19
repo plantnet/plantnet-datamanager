a047e393424da9f51f6c032f5eb85a849bff9f37
mchouet <mchouet@2504b9b7-a859-4e63-a36b-a611b0752273>
2013-04-19 13:45:25 +0000

    Small update on databases list loading message
    git-svn-id: https://amap-dev.cirad.fr/svn/p2pnote/trunk@2215 2504b9b7-a859-4e63-a36b-a611b0752273

--- evently/projects/dbs/after.js
+++ evently/projects/dbs/after.js
@@ -1,4 +1,6 @@
 function() {
-    var app = $$(this).app;
-    app.libs.utils.hideBusyMsg('databases');
+    var app = $$(this).app,
+        utilsLib = app.getlib('utils');
+
+    utilsLib.hideBusyMsg('databases');
 }
--- evently/projects/dbs/before.js
+++ evently/projects/dbs/before.js
@@ -1,4 +1,6 @@
 function() {
-    var app = $$(this).app;
-    app.libs.utils.showBusyMsg('Loading databases', 'databases').initAppData(app);
+    var app = $$(this).app,
+        utilsLib = app.getlib('utils');
+
+    utilsLib.showBusyMsg('Loading databases...', 'databases').initAppData(app);
 }
