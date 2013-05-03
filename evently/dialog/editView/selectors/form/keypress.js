ca7943515d790e5d8fe93c32ddb758556fc7dfde
mchouet <mchouet@2504b9b7-a859-4e63-a36b-a611b0752273>
2013-05-03 09:03:53 +0000

    Added feature #2904
    git-svn-id: https://amap-dev.cirad.fr/svn/p2pnote/trunk@2258 2504b9b7-a859-4e63-a36b-a611b0752273

--- evently/dialog/editView/selectors/form/keypress.js
+++
@@ -1,9 +0,0 @@
-function(e) {
-    // hit Return to submit the form (otherwise "delete field" is triggered... wtf?)
-    var key = e.keyCode || e.which;
-    if (key == 13) {
-        e.preventDefault();
-        $('#edit-view-form').submit();
-        return false;
-    }
-}
