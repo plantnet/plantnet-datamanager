61664b429d1bf2d5a2b4608accc83a972288ce1b
mchouet <mchouet@2504b9b7-a859-4e63-a36b-a611b0752273>
2013-05-06 12:57:07 +0000

    Added feature #2906
    git-svn-id: https://amap-dev.cirad.fr/svn/p2pnote/trunk@2262 2504b9b7-a859-4e63-a36b-a611b0752273

--- evently/dialog/editSyn/data.js
+++ evently/dialog/editSyn/data.js
@@ -1,37 +1,43 @@
 function (active_id, doc, syndata) {
-    var app = $$(this).app;
-    var MM = app.getlib("mm");
 
-    var active_label, doc_label, label_tpl, rows = syndata.map(
+    var app = $$(this).app,
+        active_label,
+        doc_label,
+        commonStructure;
+
+    // set multiple synonyms at once: assume that all selected docs are from the
+    // same structure (can it even be false?)
+    if (! doc) { 
+        commonStructure = syndata[0].doc.$mm;
+    }
+
+    var rows = syndata.map(
         function (e) {
-
             e.label = e.doc.$label;
-
             if(e.id === active_id) {
                 active_label = e.label;
                 e.selected = true;
             }
-            if(e.id == doc._id) {
+            if(doc && (e.id == doc._id)) {
                 doc_label = e.label;
             }
-            if(!app.libs.utils.get_parent(e.doc)) { // if not parent
+            if(!app.libs.utils.get_parent(e.doc)) { // if no parent // wtf?
                 e.disabled = true;
             }
             return e;
-        });
-
+        }
+    );
 
     rows.sortBy("label");
     var has_syn = rows.length > 1;
 
     return {
         active_id : active_id,
-        doc_id : doc._id,
-        mm : doc.$mm,
+        doc_id : (doc ? doc._id : null),
+        mm : (doc ? doc.$mm : commonStructure),
         label : doc_label,
         active_label : active_label,
         rows : rows,
         has_syn : has_syn
     };
-
-}	
+}
