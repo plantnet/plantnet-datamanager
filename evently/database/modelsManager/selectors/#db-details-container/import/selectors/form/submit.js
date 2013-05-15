0f445e7b2548cc19040edb609c38d6bccfc40a50
mchouet <mchouet@2504b9b7-a859-4e63-a36b-a611b0752273>
2013-05-15 10:20:01 +0000

    Removed redirection after import end
    git-svn-id: https://amap-dev.cirad.fr/svn/p2pnote/trunk@2273 2504b9b7-a859-4e63-a36b-a611b0752273

--- evently/database/modelsManager/selectors/#db-details-container/import/selectors/form/submit.js
+++ evently/database/modelsManager/selectors/#db-details-container/import/selectors/form/submit.js
@@ -13,11 +13,12 @@ function(evt) {
 
     function complete() {
         $('#busy-modal').modal('hide');
-        if (mm.isref) {
+        // do not redirect, so that the import report stays visible
+        /*if (mm.isref) {
             $.pathbinder.go('/tree/' + mm._id.slice(8) + '/0/0');
         } else {
             $.pathbinder.go('/viewtable/' + mm._id.slice(8) + '/0/_id/0/0/0/0/0');
-        }
+        }*/
         utilsLib.showSuccess('Import complete');
     }
 
