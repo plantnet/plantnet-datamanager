87ebeb64869249bd6dd5b7b687c2368328fa95df
mchouet <mchouet@2504b9b7-a859-4e63-a36b-a611b0752273>
2013-05-24 12:46:42 +0000

    EditDoc: if one types an incomplete date (only year or year + month), the date is completed with zeroes
    git-svn-id: https://amap-dev.cirad.fr/svn/p2pnote/trunk@2310 2504b9b7-a859-4e63-a36b-a611b0752273

--- evently/dialog/editDoc/selectors/div.edit.date/_init/after.js
+++ evently/dialog/editDoc/selectors/div.edit.date/_init/after.js
@@ -1,13 +1,20 @@
 function() {
-    /*$.datepicker.getDate = function() { // wtf?
-        return '2001-09-11';
-    }*/
-    $("input.date", this).datepicker({
+    $('input.date', this).datepicker({
         dateFormat: 'yy-mm-dd',
         showOtherMonths: true,
         constrainInput: false,
         changeYear: true,
         changeMonth: true,
         yearRange: 'c-100:c+1'
+    });
+
+    $('input.date', this).on('blur', function() {
+        var val = $(this).val();
+        if (val && val.length == 4) { // if only the year was typed, complete with zeroes
+            $(this).val(val + '-00-00');
+        }
+        if (val && val.length == 7) { // if only the year and month were typed, complete with zeroes
+            $(this).val(val + '-00');
+        }
     });
 }
